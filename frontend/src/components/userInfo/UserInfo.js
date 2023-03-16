34e9ad328b1dd8d6b00cca8b27f5a3f8a9ddab02
Joel Powell <joelhpowell@icloud.com>
2023-03-16 13:03:26 +0000

    UserInfo styling

--- frontend/src/components/userInfo/UserInfo.js
+++ frontend/src/components/userInfo/UserInfo.js
@@ -7,22 +7,22 @@ const UserInfo = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="flex">
-      <ProfilePicture className="h-32 w-32" publicId={user.imageId} />
+    <div className="flex w-max rounded-2xl border p-4 shadow-sm">
+      <ProfilePicture className="mr-4 h-32 w-32" publicId={user.imageId} />
 
-      <div>
+      <div className="flex flex-col justify-between">
         <div>
           {user.name}
-          <Button text="Edit Name" className="w-max" />
+          <Button text="Edit" className="ml-4 w-max" />
         </div>
 
         <div>
           {user.email}
-          <Button text="Edit Email" className="w-max" />
+          <Button text="Edit" className="ml-4 w-max" />
         </div>
 
         <div>
-          <Button text="Change Photo" className="w-max" />
+          <Button text="Change Photo" className="mr-4 w-max" />
           <Button text="Change Password" className="w-max" />
         </div>
       </div>
