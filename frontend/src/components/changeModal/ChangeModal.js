17e64f85371098d2a8bdbe2d3df82a29dae78243
Abigail <ajmakovsky@gmail.com>
2023-03-16 16:22:36 +0000

    ChangeModal component created - not completed

---
+++ frontend/src/components/changeModal/ChangeModal.js
@@ -0,0 +1,121 @@
+import React, { Fragment, useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { Dialog, Transition } from "@headlessui/react";
+import Button from "../button/Button";
+import { AuthContext } from "../../contexts/AuthContext";
+import { ModalContext } from "../../contexts/ModalContext";
+
+const ChangeModal = ({ isOpen, closeModal }) => {
+  const [name, setName] = useState("");
+  const { setToken, setUser } = useContext(AuthContext);
+  const { pushModal } = useContext(ModalContext);
+
+  const [isLoading, setIsLoading] = useState(null);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    const response = await fetch("/users", {
+      method: "patch",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      window.localStorage.setItem("token", data.token);
+      setToken(data.token);
+      setUser(data.user);
+      closeModal();
+    } else {
+      pushModal({
+        message: data.message,
+        type: "error",
+      });
+    }
+
+    setIsLoading(false);
+    console.log(name);
+  };
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Change your name
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Change the name you want associated with your account.
+                  </p>
+                </div>
+                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+                  <div className="space-y-4">
+                    <input
+                      placeholder="Name"
+                      id="name"
+                      type="text"
+                      required
+                      value={name}
+                      onChange={handleNameChange}
+                      className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-600"
+                    />
+                  </div>
+                  <Button
+                    text="Submit Change"
+                    type="submit"
+                    id="submit"
+                    isDisabled={isLoading}
+                  />
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+ChangeModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
+
+export default ChangeModal;
--- frontend/src/components/userInfo/UserInfo.js
+++ frontend/src/components/userInfo/UserInfo.js
@@ -1,32 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import Button from "../button/Button";
 import ProfilePicture from "../profilePicture/ProfilePicture";
+import ChangeModal from "../changeModal/ChangeModal";
 
 const UserInfo = () => {
   const { user } = useContext(AuthContext);
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
-    <div className="flex w-max rounded-2xl border p-4 shadow-sm">
-      <ProfilePicture className="mr-4 h-32 w-32" publicId={user.imageId} />
+    <>
+      <div className="flex w-max rounded-2xl border p-4 shadow-sm">
+        <ProfilePicture className="mr-4 h-32 w-32" publicId={user.imageId} />
 
-      <div className="flex flex-col justify-between">
-        <div>
-          {user.name}
-          <Button text="Edit" className="ml-4 w-max" />
-        </div>
+        <div className="flex flex-col justify-between">
+          <div>
+            {user.name}
+            <button
+              type="button"
+              onClick={openModal}
+              className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+            >
+              Edit
+            </button>
+          </div>
 
-        <div>
-          {user.email}
-          <Button text="Edit" className="ml-4 w-max" />
-        </div>
+          <div>
+            {user.email}
+            <Button text="Edit" className="ml-4 w-max" />
+          </div>
 
-        <div>
-          <Button text="Change Photo" className="mr-4 w-max" />
-          <Button text="Change Password" className="w-max" />
+          <div>
+            <Button text="Change Photo" className="mr-4 w-max" />
+            <Button text="Change Password" className="w-max" />
+          </div>
         </div>
       </div>
-    </div>
+
+      <ChangeModal isOpen={isOpen} closeModal={closeModal} />
+    </>
   );
 };
 
