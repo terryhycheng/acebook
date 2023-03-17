e48bf4abd3741e4c0dbb294be011114945f49a17
Joel Powell <joelhpowell@icloud.com>
2023-03-17 11:41:50 +0000

    add name to backend response

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -6,7 +6,7 @@ const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find()
       .sort({ createdAt: -1 })
-      .populate("author", "username imageId");
+      .populate("author", "username imageId name");
     const token = await generateToken(req.userId);
     res.status(200).json({ posts, token });
   } catch (error) {
@@ -103,7 +103,7 @@ const getPostComments = async (req, res) => {
 
     const comments = await Comment.find({ postId }).populate(
       "author",
-      "username imageId"
+      "username imageId name"
     );
     const token = await generateToken(req.userId);
 
